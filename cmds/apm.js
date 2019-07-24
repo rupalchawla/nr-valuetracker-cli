3289babab476f698bafd27386b8146c4cb22a69d
Rupal Chawla <rupal.chawla@gmail.com>
2019-07-24 09:53:17 -0700

    Removed sample code and redundand axio library

--- cmds/apm.js
+++ cmds/apm.js
@@ -1,22 +1,30 @@
 const ora = require('ora')
-const getWeather = require('../utils/weather')
-const getLocation = require('../utils/location')
+const spinner = ora({spinner: "shark",color: 'yellow', text: 'Loading'});
+// const getWeather = require('../utils/weather')
+// const getLocation = require('../utils/location')
+
+function timerFunc(arg) {
+  // console.log(`arg was => ${arg}`);
+  spinner.succeed(["Doin it"])
+  spinner.stop()
+}
 
 module.exports = async (args) => {
-  const spinner = ora().start()
+  spinner.start();
+  setTimeout(timerFunc, 4000, 'Yayoo!');
 
-  try {
-    const location = args.location || args.l || await getLocation()
-    const weather = await getWeather(location)
+  // try {
+  //   const location = args.location || args.l || await getLocation()
+  //   const weather = await getWeather(location)
 
-    spinner.stop()
+  //   spinner.stop()
 
-    console.log(`Forecast for ${location}:`)
-    weather.forecast.forEach(item =>
-      console.log(`\t${item.date} - Low: ${item.low}° | High: ${item.high}° | ${item.text}`))
-  } catch (err) {
-    spinner.stop()
+  //   console.log(`Forecast for ${location}:`)
+  //   weather.forecast.forEach(item =>
+  //     console.log(`\t${item.date} - Low: ${item.low}° | High: ${item.high}° | ${item.text}`))
+  // } catch (err) {
+  //   spinner.stop()
 
-    console.error(err)
-  }
+  //   console.error(err)
+  // }
 }
